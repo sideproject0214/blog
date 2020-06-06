854c714aa8283d7faee791556040fdbcbf477f50
sideproject0214 <sideproject@gmail.com>
2020-06-06 16:41:43 +0900

    express start

---
+++ server/app.js
@@ -0,0 +1,7 @@
+import express from "express";
+
+const app = express();
+
+app.get("/");
+
+export default app;
---
+++ server/server.js
@@ -0,0 +1,5 @@
+import app from "./app";
+
+app.listen("7000", () => {
+  console.log("hi");
+});
