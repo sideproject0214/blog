f570b2124c9a2021b763796de75b596c027eef4f
sideproject0214 <sideproject@gmail.com>
2020-06-07 07:00:38 +0900

    (01-06) Router(1)

--- server/app.js
+++ server/app.js
@@ -1,17 +1,36 @@
 import express from "express";
 import mongoose from "mongoose";
 import config from "./config";
+import hpp from "hpp";
+import helmet from "helmet";
+import cors from "cors";
+
+// Routes
+import postsRoutes from "./routes/api/post";
+import morgan from "morgan";
 
 const app = express();
 const { MONGO_URI } = config;
+
+app.use(hpp());
+app.use(helmet());
+
+app.use(cors({ origin: true, credentials: true }));
+app.use(morgan("dev"));
+
+app.use(express.json());
 
 mongoose
   .connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
   })
   .then(() => console.log("MongoDB connecting Success!!"))
   .catch((e) => console.log(e));
+
+// Use routes
 app.get("/");
+app.use("/api/post", postsRoutes);
 
 export default app;
---
+++ server/routes/api/post.js
@@ -0,0 +1,31 @@
+import express from "express";
+
+// Model
+import Post from "../../models/post";
+
+const router = express.Router();
+
+// api/post
+router.get("/", async (req, res) => {
+  const postFindResult = await Post.find();
+  console.log(postFindResult, "All Post Get");
+  res.json(postFindResult);
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    console.log(req, "req");
+    const { title, contents, fileUrl, creator } = req.body;
+    const newPost = await Post.create({
+      title,
+      contents,
+      fileUrl,
+      creator,
+    });
+    res.json(newPost);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+export default router;
