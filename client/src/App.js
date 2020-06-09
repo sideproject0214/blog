a0fcc2037165bbbac6014f9d0085a42947774af1
sideproject0214 <sideproject@gmail.com>
2020-06-10 06:27:48 +0900

    (02-02)Router Basic Setting

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Provider } from "react-redux";
+import { ConnectedRouter } from "connected-react-router";
+import store, { history } from "./store";
+import MyRouter from "./routes/Router";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <MyRouter />
+      </ConnectedRouter>
+    </Provider>
+  );
 };
 
 export default App;
---
+++ client/src/routes/Router.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+const MyRouter = () => (
+  <Fragment>
+    <Header />
+    <h1>Hello Body</h1>
+    <Footer />
+  </Fragment>
+);
+
+export default MyRouter;
