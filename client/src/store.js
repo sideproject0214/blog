5b76c1d004cf4b73b3e6de154db61ac8af0cadc3
sideproject0214 <sideproject@gmail.com>
2020-06-10 05:58:10 +0900

    (02-01)CRA & Redux

---
+++ client/src/store.js
@@ -0,0 +1,28 @@
+import { createStore, compose, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
+import { createBrowserHistory } from "history";
+import { routerMiddleware } from "connected-react-router";
+
+import createRootReducer from "./redux/reducers/index";
+import rootSaga from "./redux/sagas";
+
+export const history = createBrowserHistory();
+
+const sagaMiddleware = createSagaMiddleware();
+
+const initialState = {};
+
+const middlewares = [sagaMiddleware, routerMiddleware(history)];
+const devtools = window.__REDUX_DEVTOOLS_EXTENSIONS_COMPOSE__;
+
+const composeEnhancer =
+  process.env.NODE_ENV === "production" ? compose : devtools || compose;
+
+const store = createStore(
+  createRootReducer(history),
+  initialState,
+  composeEnhancer(applyMiddleware(...middlewares))
+);
+sagaMiddleware.run(rootSaga);
+
+export default store;
