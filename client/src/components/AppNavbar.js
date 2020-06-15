5e7ceee141954233eaff30156924668bbfad02fa
sideproject0214 <sideproject@gmail.com>
2020-06-16 06:29:29 +0900

    (02-09)Header-Register Modal(1)

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -1,9 +1,19 @@
 import React, { Fragment, useState, useCallback, useEffect } from "react";
-import { Navbar, Container, NavbarToggler, Collapse, Nav } from "reactstrap";
+import {
+  Navbar,
+  Container,
+  NavbarToggler,
+  Collapse,
+  Nav,
+  NavItem,
+  Form,
+  Button,
+} from "reactstrap";
 import { Link } from "react-router-dom";
 import LoginModal from "../components/auth/LoginModal";
 import { useSelector, useDispatch } from "react-redux";
 import { LOGOUT_REQUEST } from "../redux/types";
+import RegisterModal from "./auth/RegisterModal";
 
 const AppNavbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -28,6 +38,63 @@ const AppNavbar = () => {
     setIsOpen(!isOpen);
   };
 
+  const addPostClick = () => {};
+
+  const authLink = (
+    <Fragment>
+      <NavItem>
+        {userRole === "MainJuin" ? (
+          <Form className="col mt-2">
+            <Link
+              to="posts"
+              className="btn btn-success block text-white px-3"
+              onClick={addPostClick}
+            >
+              Add Post
+            </Link>
+          </Form>
+        ) : (
+          ""
+        )}
+      </NavItem>
+      <NavItem className="d-flex justify-content-center">
+        <Form className="col mt-2">
+          {user && user.name ? (
+            <Link>
+              <Button outline color="light" className="px-3" block>
+                <strong>{user ? `Welcome ${user.name}` : ""}</strong>
+              </Button>
+            </Link>
+          ) : (
+            <Button outline color="light" className="px-3" block>
+              <strong>No User</strong>
+            </Button>
+          )}
+        </Form>
+      </NavItem>
+      <NavItem>
+        <Form className="col">
+          <Link onClick={onLogout} to="#">
+            <Button outline color="light" className="mt-2" block>
+              Logout
+            </Button>
+          </Link>
+        </Form>
+      </NavItem>
+    </Fragment>
+  );
+
+  const guestLink = (
+    <Fragment>
+      <NavItem>
+        <RegisterModal />
+      </NavItem>
+      <NavItem>
+        <LoginModal />
+      </NavItem>
+    </Fragment>
+  );
+
   return (
     <Fragment>
       <Navbar color="dark" dark expand="lg" className="sticky-top">
@@ -38,11 +105,7 @@ const AppNavbar = () => {
           <NavbarToggler onClick={handleToggle} />
           <Collapse isOpen={isOpen} navbar>
             <Nav className="ml-auto d-felx justify-content-around" navbar>
-              {isAuthenticated ? (
-                <h1 className="text-white">authLink</h1>
-              ) : (
-                <LoginModal />
-              )}
+              {isAuthenticated ? authLink : guestLink}
             </Nav>
           </Collapse>
         </Container>
