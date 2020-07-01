ce12bcd66acbaf7604676268e7d49d4a6ed07740
sideproject0214 <sideproject@gmail.com>
2020-07-02 05:34:10 +0900

    (02-21)Post Upload Saga

--- client/src/redux/sagas/postSaga.js
+++ client/src/redux/sagas/postSaga.js
@@ -5,6 +5,9 @@ import {
   POSTS_LOADING_FAILURE,
   POSTS_LOADING_SUCCESS,
   POSTS_LOADING_REQUEST,
+  POST_UPLOADING_SUCCESS,
+  POST_UPLOADING_FAILURE,
+  POST_UPLOADING_REQUEST,
 } from "../types";
 
 // All Posts load
@@ -34,6 +37,44 @@ function* watchLoadPosts() {
   yield takeEvery(POSTS_LOADING_REQUEST, loadPosts);
 }
 
+// Post Upload
+
+const uploadPostAPI = (payload) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  const token = payload.token;
+  if (token) {
+    config.headers["x-auth-token"] = token;
+  }
+  return axios.get("/api/post", payload, config);
+};
+
+function* uploadPosts(action) {
+  try {
+    console.log(action, "uploadPost function");
+    const result = yield call(uploadPostAPI, action.payload);
+    console.log(result, "uploadPostAPI, action.payload");
+    yield put({
+      type: POST_UPLOADING_SUCCESS,
+      payload: result.data,
+    });
+    yield put(push(`/post/${result.data._id}`));
+  } catch (e) {
+    yield put({
+      type: POST_UPLOADING_FAILURE,
+      payload: e,
+    });
+    yield push("/");
+  }
+}
+
+function* watchuploadPosts() {
+  yield takeEvery(POST_UPLOADING_REQUEST, uploadPosts);
+}
+
 export default function* postSaga() {
-  yield all([fork(watchLoadPosts)]);
+  yield all([fork(watchLoadPosts), fork(watchuploadPosts)]);
 }
--- client/src/routes/normalRoute/PostWrite.js
+++ client/src/routes/normalRoute/PostWrite.js
@@ -13,6 +13,7 @@ import CKEditor from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-editor-classic/src/classiceditor";
 import { editorConfiguration } from "../../components/editor/EditorConfig";
 import Myinit from "../../components/editor/UploadAdapter";
+import { POST_UPLOADING_REQUEST } from "../../redux/types";
 
 import dotenv from "dotenv";
 dotenv.config();
@@ -21,6 +22,17 @@ const PostWrite = () => {
   const { isAuthenticated } = useSelector((state) => state.auth);
   const [form, setValues] = useState({ title: "", contents: "", fileUrl: "" });
   const dispatch = useDispatch();
+
+  const onSubmit = async (e) => {
+    await e.preventDefault();
+    const { title, contents, fileUrl, category } = form;
+    const token = localStorage.getItem("token");
+    const body = { title, contents, fileUrl, category, token };
+    dispatch({
+      type: POST_UPLOADING_REQUEST,
+      payload: body,
+    });
+  };
 
   const onChange = (e) => {
     setValues({
@@ -76,7 +88,7 @@ const PostWrite = () => {
   return (
     <div>
       {isAuthenticated ? (
-        <Form>
+        <Form onSubmit={onSubmit}>
           <FormGroup className="mb-3">
             <Label for="title">Title</Label>
             <Input
