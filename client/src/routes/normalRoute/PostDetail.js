687931aecfff6c1c111a4b3d07bf244b492036ee
sideproject0214 <sideproject@gmail.com>
2020-07-03 06:05:23 +0900

    (02-22)Post Detail(1)

--- client/src/routes/normalRoute/PostDetail.js
+++ client/src/routes/normalRoute/PostDetail.js
@@ -1,6 +1,80 @@
-import React from "react";
+import React, { useEffect, Fragment } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {} from "react-helmet";
+import {
+  POST_DETAIL_LOADING_REQUEST,
+  POST_DELETE_REQUEST,
+  USER_LOADING_REQUEST,
+} from "../../redux/types";
+import { Button } from "reactstrap";
+import { Link } from "react-router-dom";
+import CKEditor from "@ckeditor/ckeditor5-react";
 
-const PostDetail = () => {
+const PostDetail = (req) => {
+  const dispatch = useDispatch();
+  const { postDetail, creatorId, title, loading } = useSelector(
+    (state) => state.post
+  );
+  const { userId, userName } = useSelector((state) => state.auth);
+  console.log(req);
+  useEffect(() => {
+    dispatch({
+      type: POST_DETAIL_LOADING_REQUEST,
+      payload: req.match.params.id,
+    });
+    dispatch({
+      type: USER_LOADING_REQUEST,
+      payload: localStorage.getItem("token"),
+    });
+  });
+
+  const onDeleteClick = () => {
+    dispatch({
+      type: POST_DELETE_REQUEST,
+      payload: {
+        id: req.match.params.id,
+        token: localStorage.getItem("token"),
+      },
+    });
+  };
+
+  const EditButton = (
+    <Fragment>
+      <Row className="d-flex justify-content-center pb-3">
+        <Col className="col-md-3 mr-md-3">
+          <Link to="/" className="btn btn-primary btn-block">
+            Home
+          </Link>
+        </Col>
+        <Col className="col-md-3 mr-md-3">
+          <Link
+            to={`/post/${req.match.parasm.id}/edit`}
+            className="btn btn-success btn-block"
+          >
+            Edit Post
+          </Link>
+        </Col>
+        <Col className="col-md-3">
+          <Button className="btn-block btn-danger" onClick={onDeleteClick}>
+            Delete
+          </Button>
+        </Col>
+      </Row>
+    </Fragment>
+  );
+
+  const HomeButton = (
+    <Fragment>
+      <Row className="d-flex justify-content-center pb-3">
+        <Col className="col-sm-12 com-md-3">
+          <Link to="/" className="btn btn-primary btn-block">
+            Home
+          </Link>
+        </Col>
+      </Row>
+    </Fragment>
+  );
+
   return <h1>PostDetail</h1>;
 };
 
